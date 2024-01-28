2631e94aa097429179a18184cd0f94f7022ef3e5
Yunseo Lee <yunseolee.dev@gmail.com>
2024-01-29 02:43:17 +0900

    [UPDATE] update list, put, get commands

---
+++ client/fshare.c
@@ -0,0 +1,211 @@
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <arpa/inet.h>
+#include <string.h> 
+#include <unistd.h>
+
+#define BUF_SIZE 512
+
+#ifdef DEBUG
+	#define debug(fn) fn
+#else
+	#define debug(fn)
+#endif
+
+typedef enum {
+    LIST,
+    GET,
+    PUT,
+    N_OP
+} commands;
+
+char *op_str[N_OP] = {
+	"list",
+	"get",
+	"put"
+};
+
+int recv_bytes(int sock, void *buf, size_t len){
+	char *p = (char *)buf;
+	int acc = 0;
+	int received;
+	while(acc < len){
+		received = recv(sock, p, len-acc, 0);
+		if(received == 0 || received == -1){
+			return 1;
+		}
+		acc += received;
+		p += received;
+	}
+	return 0;
+}
+
+int send_bytes(int sock, void *buf, size_t len){
+	char *p = (char *)buf;
+	int acc = 0;
+	int sent;
+	while(acc < len){
+		sent = send(sock, p, len-acc, MSG_NOSIGNAL);
+		if(sent == 0 || sent == -1){
+			return 1;
+		}
+		acc += sent;
+		p += sent;
+	}
+	return 0;
+}
+
+commands 
+get_cmd(char *command)
+{
+	for(int i=0;i<N_OP;i++){
+		if(strcmp(command,op_str[i])==0){
+			return (commands) i;
+		}
+	}
+	return N_OP;
+}
+
+void
+recv_list(int sock_fd)
+{
+	size_t name_len;
+	while(1){
+		if(recv_bytes(sock_fd,(void *)&name_len,sizeof(name_len))) break;
+		char *name = (char *)malloc(name_len+1);
+		if(recv_bytes(sock_fd,(void *)name,name_len)){
+			fprintf(stderr,"recv name error");
+			return;
+		}
+		name[name_len] = '\0';
+		printf("%s\n",name);
+	}
+}
+
+void
+download_file(int sock_fd, char *filename)
+{
+	send_bytes(sock_fd,(void *)filename,strlen(filename)+1);
+	shutdown(sock_fd, SHUT_WR);
+
+	long long filesize;
+	recv_bytes(sock_fd,(void *)&filesize,sizeof(filesize));
+	if(filesize == -1){
+		fprintf(stderr,"There is no file named %s\n",filename);
+		return;
+	}
+	debug(printf("filesize: %lld\n",filesize));
+
+	FILE *fp = fopen(filename,"wb");
+	char buf[BUF_SIZE];
+	int received;
+	long long count = 0;
+	while(count < filesize){
+		received = recv(sock_fd,buf,sizeof(buf),0);
+		if(received <= 0) break;
+		fwrite(buf,1,received,fp);
+		count += received;
+	}
+	fclose(fp);
+	printf("[%d] %s download\n",sock_fd,filename);
+}
+
+void 
+upload_file(int sock_fd, char *filename)
+{
+	FILE *fp = fopen(filename,"rb");
+	if(fp == NULL){
+		perror("fopen error");
+		return;
+	}
+	size_t filename_len = strlen(filename);
+	send_bytes(sock_fd,(void *)&filename_len,sizeof(filename_len));
+	send_bytes(sock_fd,(void *)filename,filename_len);
+	char buf[BUF_SIZE];
+	size_t bytesRead;
+	while((bytesRead = fread(buf,1,sizeof(buf),fp)) > 0){
+		send_bytes(sock_fd,(void *)buf,bytesRead);
+	}
+	shutdown(sock_fd,SHUT_WR);
+	fclose(fp);
+	printf("[%d] %s uploaded\n",sock_fd,filename);
+}
+
+#ifdef MAIN
+int 
+main(int argc, char *argv[]) 
+{ 
+	if(argc < 3){
+		fprintf(stderr,"Usage: %s [ip_addr:port_num] [command]\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	struct sockaddr_in serv_addr; 
+	int sock_fd ;
+	char * data ;
+	int port_num;
+	char *ip_addr;
+	commands cmd;
+
+	sock_fd = socket(AF_INET, SOCK_STREAM, 0) ;
+	if (sock_fd <= 0) {
+		perror("socket failed") ;
+		exit(EXIT_FAILURE) ;
+	}
+
+	ip_addr = strtok(argv[1],":");
+	if(ip_addr == NULL){
+		fprintf(stderr, "Invalid IP address\n Usage: %s [ip_addr:port_num] [command]\n",argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	sscanf(strtok(NULL,":"),"%d",&port_num);
+	if(port_num < 1024 || port_num > 49151){
+		fprintf(stderr, "Invalid port number\n Usage: %s [ip_addr:port_num] [command]\n",argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	debug(printf("IP address: %s\n",ip_addr));
+	debug(printf("Port Number: %d\n",port_num));
+	debug(printf("User Command: %s\n",argv[2]));
+
+	cmd = get_cmd(argv[2]);
+	if(cmd == N_OP){
+		fprintf(stderr, "Invalid command\n");
+		exit(EXIT_FAILURE);
+	}
+	debug(printf("cmd: %d\n",cmd);)
+
+	memset(&serv_addr, '\0', sizeof(serv_addr)); 
+	serv_addr.sin_family = AF_INET; 
+	serv_addr.sin_port = htons(port_num); 
+	if (inet_pton(AF_INET, ip_addr, &serv_addr.sin_addr) <= 0) {
+		perror("inet_pton failed") ; 
+		exit(EXIT_FAILURE) ;
+	} 
+
+	if (connect(sock_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+		perror("connect failed") ;
+		exit(EXIT_FAILURE) ;
+	}
+	
+	if(send_bytes(sock_fd,(void *)&cmd,sizeof(cmd)) == 1){
+		perror("send_bytes error");
+		exit(EXIT_FAILURE);
+	}
+	
+	switch(cmd){
+		case LIST:
+			recv_list(sock_fd);
+			break;
+		case GET:
+			if(argc < 4) fprintf(stderr,"No file name\n");
+			else download_file(sock_fd, argv[3]);
+			break;
+		case PUT:
+			if(argc < 4) fprintf(stderr,"No file name\n");
+			else upload_file(sock_fd, argv[3]);
+	}
+	close(sock_fd);
+} 
+#endif
---
+++ server/fshared.c
@@ -0,0 +1,282 @@
+// Partly taken from https://www.geeksforgeeks.org/socket-programming-cc/
+
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#include <pthread.h>
+#include <dirent.h>
+#include <signal.h>
+#include <sys/stat.h>
+
+#define FILE_NAME_SIZE 256
+#define FILE_PATH_SIZE 512
+#define BUF_SIZE 512
+
+#ifdef DEBUG
+	#define debug(fn) fn
+#else
+	#define debug(fn)
+#endif
+
+typedef enum{
+	LIST,
+	GET,
+	PUT,
+	N_OP
+}commands;
+
+char *dir_path;
+DIR *dir;
+pthread_mutex_t dir_lock = PTHREAD_MUTEX_INITIALIZER;
+
+void 
+signal_handler(int sig)
+{
+	if(sig == SIGINT){
+		printf("close dir\n");
+		closedir(dir);
+		printf("exit success\n");
+		exit(EXIT_SUCCESS);
+	}
+}
+
+int 
+recv_bytes(int sock, void *buf, size_t len)
+{
+	char *p = (char *)buf;
+	int acc = 0;
+	int received;
+	while(acc < len){
+		received = recv(sock, p, len-acc, 0);
+		if(received == 0 || received == -1){
+			return 1;
+		}
+		acc += received;
+		p += received;
+	}
+	return 0;
+}
+
+int 
+send_bytes(int sock, void *buf, size_t len)
+{
+	char *p = (char *)buf;
+	int acc = 0;
+	int sent;
+	while(acc < len){
+		sent = send(sock, p, len-acc, 0);
+		if(sent == 0 || sent == -1){
+			return 1;
+		}
+		acc += sent;
+		p += sent;
+	}
+	return 0;
+}
+
+void
+send_list(int sock_fd)
+{
+	pthread_mutex_lock(&dir_lock);
+	struct dirent *entry;
+	rewinddir(dir);
+	while((entry = readdir(dir)) != NULL){
+		if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+			continue;
+		}
+		char *name = entry->d_name;
+		size_t name_len = strlen(name);
+		debug(printf("%s : %ld\n",name,name_len);)
+		if(send_bytes(sock_fd,(void *)&name_len,sizeof(name_len))){
+			fprintf(stderr,"send name_len error\n");
+			return;
+		}
+		if(send_bytes(sock_fd,(void *)name,strlen(name))){
+			fprintf(stderr,"send name error\n");
+			return;
+		}
+	}
+	shutdown(sock_fd,SHUT_WR);
+	pthread_mutex_unlock(&dir_lock);
+	printf("[%d] send list success\n",sock_fd);
+}
+
+void 
+send_file(int sock_fd)
+{
+	char filename[FILE_NAME_SIZE];
+	recv_bytes(sock_fd,(void *)filename,sizeof(filename));
+	pthread_mutex_lock(&dir_lock);
+	char filepath[FILE_PATH_SIZE];
+	struct stat filestat;
+	snprintf(filepath,sizeof(filepath),"%s/%s",dir_path,filename);
+	debug(printf("sending %s...\n",filepath));
+
+	long long filesize;
+	if(stat(filepath,&filestat) == 0){
+		if (S_ISREG(filestat.st_mode)){
+			filesize = filestat.st_size;
+		}
+		else filesize = -1;
+	}
+	else{
+		perror("stat error");
+		filesize = -1;
+	}
+	
+	// send filesize
+	debug(printf("filesize: %lld\n",filesize);)
+	send_bytes(sock_fd,(void *)&filesize,sizeof(filesize));
+	if(filesize == -1){
+		pthread_mutex_unlock(&dir_lock);
+		return;
+	}
+
+	//send file
+	FILE *fp = fopen(filepath,"rb");
+	char buf[BUF_SIZE];
+	size_t bytesRead;
+	while((bytesRead = fread(buf,1,sizeof(buf),fp)) > 0){
+		send_bytes(sock_fd,(void *)buf,bytesRead);
+	}
+	fclose(fp);
+	pthread_mutex_unlock(&dir_lock);
+	printf("[%d] send %s success\n",sock_fd,filepath);
+}
+
+void 
+recv_file(int sock_fd)
+{
+	size_t filename_len;
+	if(recv_bytes(sock_fd,(void *)&filename_len,sizeof(filename_len))){
+		fprintf(stderr,"failed to receive file\n");
+		return;
+	}
+	char filename[filename_len+1];
+	recv_bytes(sock_fd,(void *)filename,filename_len);
+	filename[filename_len] = '\0';
+	char filepath[FILE_PATH_SIZE];
+	snprintf(filepath,sizeof(filepath),"%s/%s",dir_path,filename);
+
+	pthread_mutex_lock(&dir_lock);
+	FILE *fp = fopen(filepath,"wb");
+	char buf[BUF_SIZE];
+	int received;
+	long long count = 0;
+	while((received= recv(sock_fd,buf,sizeof(buf),0)) > 0){
+		fwrite(buf,1,received,fp);
+	}
+	fclose(fp);
+	pthread_mutex_unlock(&dir_lock);
+	printf("[%d] reveive %s success\n",sock_fd,filepath);
+}
+
+void *
+handle_client(void *sock)
+{
+  int conn = *((int *)sock);
+	int cmd;
+	char buf[1024] = {0};
+
+	if(recv_bytes(conn, (void *)&cmd, sizeof(cmd))){
+		fprintf(stderr,"recv cmd error\n");
+		close(conn);
+		return NULL;
+	}
+	debug(printf("cmd: %d\n", cmd);)
+
+	switch(cmd){
+		case LIST:
+			send_list(conn);
+			break;
+		case GET:
+			send_file(conn);
+			break;
+		case PUT:
+			recv_file(conn);
+	}
+	close(conn);
+}
+
+#ifdef MAIN
+int 
+main(int argc, char *argv[])
+{
+	int opt;
+	int port_num;
+
+	//Parsing command line arguments
+	if(argc < 5){
+		fprintf(stderr, "Usage: %s [-p port_num] [-d dir_path]\n", argv[0]);
+        return 1;	
+	}
+	while((opt = getopt(argc, argv, "p:d:")) != -1){
+		switch(opt){
+			case 'p':
+				sscanf(optarg, "%d\n", &port_num);
+				if(port_num < 1024 || port_num > 49151){
+					fprintf(stderr,"Invalid port number");
+					exit(EXIT_FAILURE);
+				}
+				break;
+			case 'd':
+				dir_path = (char *)malloc(strlen(optarg));
+				memcpy(dir_path, optarg, strlen(optarg));
+				break;
+			default:
+				fprintf(stderr, "Usage: %s [-p port_num] [-d dir_path]\n", argv[0]);
+				return 1;
+		}
+	}
+	debug(printf("port num: %d\n",port_num));
+	debug(printf("dir path: %s\n",dir_path));
+	dir = opendir(dir_path);
+	if(dir == NULL){
+		perror("Cannot open directory");
+		exit(EXIT_FAILURE);
+	}
+
+	signal(SIGINT, signal_handler);
+
+  pthread_t tid;
+	int listen_fd, new_socket ; 
+	struct sockaddr_in address; 
+	int addrlen = sizeof(address); 
+
+	//Make connection
+	listen_fd = socket(AF_INET /*IPv4*/, SOCK_STREAM /*TCP*/, 0 /*IP*/) ;
+	if (listen_fd == 0)  { 
+		perror("socket failed : "); 
+		exit(EXIT_FAILURE); 
+	}
+	
+	memset(&address, '0', sizeof(address)); 
+	address.sin_family = AF_INET; 
+	address.sin_addr.s_addr = INADDR_ANY /* the localhost*/ ; 
+	address.sin_port = htons(port_num); 
+	if (bind(listen_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+		perror("bind failed : "); 
+		exit(EXIT_FAILURE); 
+	} 
+
+	while (1) {
+		if (listen(listen_fd, 16 /* the size of waiting queue*/) < 0) { 
+			perror("listen failed : "); 
+			exit(EXIT_FAILURE); 
+		} 
+
+		new_socket = accept(listen_fd, (struct sockaddr *) &address, (socklen_t*)&addrlen) ;
+		if (new_socket < 0) {
+			perror("accept"); 
+			exit(EXIT_FAILURE); 
+		}
+
+		if(pthread_create(&tid,NULL,handle_client,&new_socket)!=0){
+				perror("pthread create error");
+		}
+	}
+} 
+#endif
